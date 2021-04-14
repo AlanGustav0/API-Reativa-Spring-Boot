c82da61badde883e27d9a7bf02bfe3b2389d4057
Alan Gustavo <alangustavo946@gmail.com>
2021-04-14 19:00:20 -0300

    Segundo Commit

---
+++ apiheroes/src/main/java/com/digitalinnovationone/apiheroes/ApiheroesApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.apiheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiheroesApplication.class, args);
+	}
+
+}
