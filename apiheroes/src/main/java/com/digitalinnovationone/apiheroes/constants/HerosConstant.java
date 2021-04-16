3813970d360c1ccb13ed916261367b5e1b2481a0
Alan Gustavo <alangustavo946@gmail.com>
2021-04-16 19:02:47 -0300

    Segundo Commit

--- apiheroes/src/main/java/com/digitalinnovationone/apiheroes/constants/HerosConstant.java
+++ apiheroes/src/main/java/com/digitalinnovationone/apiheroes/constants/HerosConstant.java
@@ -4,5 +4,4 @@ public class HerosConstant {
 
     public static final String HEROES_ENDPOINT_LOCAL="/heroes";
     public static final String ENDPOINT_COSMOS="http://localhost:8080";
-    public static final String REGION_COSMOS="us-east-1";
 }
